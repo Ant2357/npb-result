95cd74220cea861cbefd9e4637ac46eca933a355
Ant2357 <ryu4979@gmail.com>
2024-12-11 15:15:56 +0900

    fix: 順位結果に文字列「優勝」が入ったケースにて、順位が正しく返されない問題の修正

--- src/npb/baseballTeam.ts
+++ src/npb/baseballTeam.ts
@@ -1,6 +1,6 @@
 
 export class BaseballTeam {
-  rank: number;
+  rank: number | string;
   name: string;
   playGameCount: number;
   win: number;
@@ -28,8 +28,8 @@ export class BaseballTeam {
   /**
    *Creates an instance of BaseballTeam.
    */
-  constructor(rank = 0, name = "", playGameCount = 0, win = 0, lose = 0, draw = 0, pct = 0.0, gamesBehind = "", remainingGames = 0, run = 0, ra = 0, hr = 0, sb = 0, avg = 0.0, era = 0.0, e = 0) {
-    this.rank = rank;
+  constructor(rank: number | string = 0, name = "", playGameCount = 0, win = 0, lose = 0, draw = 0, pct = 0.0, gamesBehind = "", remainingGames = 0, run = 0, ra = 0, hr = 0, sb = 0, avg = 0.0, era = 0.0, e = 0) {
+    this.rank = rank === "優勝" ? 1 : rank;
     this.name = name;
     this.playGameCount = playGameCount;
     this.win = win;
